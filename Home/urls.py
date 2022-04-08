d33dcfde896381fa2b9b80d2136d0f358a227112
DmitrI2488 <povarovvv48@gmail.com>
2022-04-08 19:50:00 +0300

    1.1

--- Home/urls.py
+++ Home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.all_events, name='home'),
+    path('chanel/<chanel_id>', views.show_chanel, name="page"),
+    path('search_chanel', views.search_chanel, name='search')
 ]
--- Home/views.py
+++ Home/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 from Home.models import rating
 
+from django.core.paginator import Paginator
+
+
+def show_chanel(request, chanel_id):
+    chanel = rating.objects.get(pk=chanel_id)
+    return render(request, 'Home/show_chanel.html', {
+        'chanel': chanel
+    })
+
 
 def all_events(request):
     rating_list = rating.objects.all()
@@ -10,3 +19,24 @@ def all_events(request):
     return render(request, 'Home/home.html', {
         'rating_list': rating_list
     })
+
+
+def search_chanel(request):
+    if request.method == 'POST':
+        search = request.POST['search']
+        searched = rating.objects.filter(name__contains=search)
+        p = Paginator(rating.objects.filter(name__contains=search), 2)
+        page = request.GET.get('page')
+        chanels = p.get_page(page)
+        return render(request, 'Home/search_chanel.html',
+                      {'search': search,
+                       'results': searched,
+                       'chanels': chanels})
+    if request.method == 'GET':
+        p = Paginator(rating.objects.filter(name__contains=search), 2)
+        page = request.GET.get('page')
+        chanels = p.get_page(page)
+        return render(request, 'Home/search_chanel.html',
+                      {'search': search,
+                       'results': searched,
+                       'chanels': chanels})
